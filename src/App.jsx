5a9ac9d90c51867b44d95ee037ba454bd13bd8f8
Augusto Firmino <gugufirmino1@gmail.com>
2025-07-16 10:00:49 +0100

    trabalhando na tela de login

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { BrowserRouter, Routes,Router, Route, Link } from 'react-router-dom';
 import Home from './pagina/inicio'
 import Biografia from './pagina/bibiografia'
 import Cadastro_Directores from './pagina/cadastrar_directores'
+import Login from './pagina/login'
 import './App.css';
 function App() {
   return (
@@ -18,8 +19,9 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} /> {/* Rota para Home */}
-        <Route path="/biografia/:nome/:id" element={<Biografia />} /> {/* Rota para Home */}
-       <Route path="/cadastrar_directores" element={<Cadastro_Directores/>} /> {/* Rota para Home */}
+        <Route path="/biografia/:nome/:id" element={<Biografia />} /> {/* Rota para Biografia */}
+       <Route path="/cadastrar_directores" element={<Cadastro_Directores/>} /> {/* Rota para Cadastro pelo admin */}
+          <Route path="/login" element={<Login/>} /> {/* Rota para login pelo admin */}
       </Routes>
 
     </BrowserRouter>
